f8b80847387e43f85c60486779db185bad07e2d9
xiaolittle <longyan214@gmail.com>
2014-01-06 21:57:20 -0800

    Create Candy.cpp
    leetcode Candy

---
+++ lc/Candy.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int candy(vector<int> &ratings) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        int n = ratings.size();
+        if(n == 0) {
+            return 0;
+        } else if(n == 1) {
+            return 1;
+        }
+        vector<int> ans(n, 1);
+        for(int i = 1; i < n; ++i) {
+            if(ratings[i] > ratings[i - 1]) {
+                ans[i] = ans[i - 1] + 1;
+            }
+        }
+        int sum = ans[n - 1];
+        for(int i = n - 2; i >= 0; --i) {
+            if(ratings[i] > ratings[i + 1]) {
+                ans[i] = max(ans[i], ans[i + 1] + 1);
+            }
+            sum += ans[i];
+        }
+        return sum;
+    }
+};
